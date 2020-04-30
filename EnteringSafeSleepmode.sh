1795293421ade3d497dcfcc677fc425e0da2fd8e
cdr-chakotay <60937022+cdr-chakotay@users.noreply.github.com>
2020-05-01 01:41:48 +0200

    Add files via upload

---
+++ EnteringSafeSleepmode.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+read -p "Do you want to unmount external drives and hibbernate? (y/n)" var1
+
+var1=`echo $var1 | tr '[:upper:]' '[:lower:]'`
+
+if [ $var1 = "y" ] || [ $var1 = "n" ] || [ $var1 = "j" ]
+then
+echo ""
+else
+echo "Wrong input .... please type (y)es or (n)o .... stopping the program"
+sleep 3
+exit 1
+fi
+
+if [ $var1 = "n" ]
+then
+echo "Stopping Process"
+sleep 3
+exit 0
+fi
+
+echo " Unmounting Disks ...."
+
+countdrives=`diskutil list | grep -c "/disk"`
+
+#usallay two paritions in use
+
+if [ $countdrives -gt 3 ]
+then
+  #Mac has /disk0 and /disk1 in use. Counting starts at Zero.... -1 corrects the factor for loop
+  let countdrives=countdrives-1
+
+#Building realistic number for output (-1)
+  tounmount=$countdrives
+  let tounmount=tounmount-1
+
+  echo "$tounmount Volumes to unmount ..."
+
+  while [ $countdrives -gt 1 ]
+  do
+  diskutil unmountDisk /dev/disk$countdrives
+  let countdrives=countdrives-1
+  done
+fi
+
+echo "Unmounting drives complete"
+sleep 1
+
+echo "Entering sleepmode.... "
+sleep 2
+pmset sleepnow
+
+exit 0
